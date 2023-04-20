f204c5facd65e9a2449a5a773748bdca460eb8dd
pippix <pippianders@hotmail.com>
2023-04-20 22:43:39 +0200

    /*  ============================================================================  Name        : ground.c  Author      : Pippi A. Tednes  Version     :  Copyright   : Anarchy says fuck you!  Description : Hello World in C, Ansi-style  ============================================================================  */
    #include <stdio.h>
    #include <stdlib.h>
    
    int main(void) {
    	puts("!!!Hello Ground!!!"); /* prints !!!Hello Ground!!! */
    	return EXIT_SUCCESS;
    }

---
+++ ground.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ground.c
+ Author      : Pippi A. Tednes
+ Version     :
+ Copyright   : Anarchy says fuck you!
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello Ground!!!"); /* prints !!!Hello Ground!!! */
+	return EXIT_SUCCESS;
+}
